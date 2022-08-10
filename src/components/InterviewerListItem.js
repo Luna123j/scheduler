7d0ac745c85fd454e15d87bd977c6966150a6a96
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-09 20:45:45 -0400

    create intervierItemList and test in storybook

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/InterviewerListItem.scss"
+import classNames from "classnames";
+
+function InterviewerListItem(props){
+
+  let interviwerClass = classNames('li',{
+    'interviewers__item': !props.selected,
+    'interviewers__item--selected' : props.selected}
+  )
+  
+
+ return (<li onClick={()=>{props.setInterviewer(props.id)}} className={interviwerClass}>
+ <img
+   className="interviewers__item-image"
+   src={props.avatar}
+   alt={props.name}
+ />
+ {props.selected ? props.name : ""}
+</li>)
+}
+
+export default InterviewerListItem;
