de60df34d0b0913e38508b3d2d92eaef38c8a354
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-10 23:06:57 -0400

    change the hard coded days array to the date get from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "components/Appointment"
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+import axios from "axios";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = {
   "1": {
@@ -63,6 +48,12 @@ const appointments = {
 
 export default function Application(props) {
  const [daystate,setDaystate] = useState('Monday');
+ const [days, setDays] = useState([]);
+ useEffect(()=>{
+   axios.get('/api/days').then((response)=>{
+     setDays([...response.data])
+   })
+ },[days])
 
   return (
     <main className="layout">
