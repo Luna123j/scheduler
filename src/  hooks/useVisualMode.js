e07f4629c4ee2e68bad457efcdecfe9ea8f68a65
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-11 20:14:19 -0400

    transition can replace the previous mode with current mode if replace is required

--- src/  hooks/useVisualMode.js
+++ src/  hooks/useVisualMode.js
@@ -4,15 +4,20 @@ import React, { useState } from "react";
 function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  function transition(newMode) {
+  function transition(newMode, condition = false) {
+    if (condition) {
+      setHistory(prev => [...(prev.slice(0, -1)),newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
     setMode(newMode);
-    setHistory(prev => [...prev, newMode]);
+
   };
 
   function back() {
     if (history.length > 1) {
       setMode(history[history.length - 2]);
-      setHistory(prev => [...prev.slice(0,-1)]);
+      setHistory(prev => [...(prev.slice(0, -1))]);
     }
   }
 
