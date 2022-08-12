a55d6bf31ae624cdbf2c5bf874dd1391880ae6d2
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-11 20:00:29 -0400

    add back function to let mode go back

--- src/  hooks/useVisualMode.js
+++ src/  hooks/useVisualMode.js
@@ -1,12 +1,22 @@
-import React,{useState}from "react";
+import React, { useState } from "react";
 
 
-function useVisualMode(initial){
-  const [mode, setMode] = useState(initial)
-  function transition(newMode){
-     setMode(newMode);
+function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  function transition(newMode) {
+    setMode(newMode);
+    setHistory(prev => [...prev, newMode]);
   };
-  return {transition,mode};
+
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory(prev => [...prev.slice(0,-1)]);
+    }
+  }
+
+  return { transition, mode, back };
 }
 
-export {useVisualMode};
+export { useVisualMode };
